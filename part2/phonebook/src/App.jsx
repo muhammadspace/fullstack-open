570c0839c809a83594c83a7e92bb976ab7164c77
muhammadv-i <76049040+muhammadv-i@users.noreply.github.com>
2023-08-31 07:06:51 +0000

    phonebook part2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect( () => {
+    axios
+      .get("https://probable-potato-p677x67prgrfvjw-3001.app.github.dev/persons")
+      .then( res => setPersons(res.data))
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
