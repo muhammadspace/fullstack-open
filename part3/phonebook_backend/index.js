62b52e74dc37423f5871dd2870417e0a7fd1b725
muhammadv-i <76049040+muhammadv-i@users.noreply.github.com>
2023-09-06 15:29:05 +0000

    rebuilt and deployed app

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan( (tokens, req, res) => {
