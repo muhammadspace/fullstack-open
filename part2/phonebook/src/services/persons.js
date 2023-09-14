ca336f8bb5666b07da37aa79ffc849f4f339bca5
Mohammad Magd <mohamed10.sw2021@fci.helwan.edu.eg>
2023-09-15 01:48:09 +0300

    phonebook: frontend now connects to process.env.PORT instead of hardcoded 3001

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const PORT = process.env.PORT || 3001
+const baseUrl = `http://localhost:${PORT}/api/persons`
 
 const getAll = () => {
     return axios
