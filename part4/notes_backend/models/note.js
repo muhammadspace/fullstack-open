d47a6e0a7b3584f122874033722311126a5e7bd8
Mohammad Magd <mohamed10.sw2021@fci.helwan.edu.eg>
2023-09-16 00:51:08 +0300

    added part4 directory and rolled part3/notes_backend back to actually reflect part3 and not part4

---
+++ part4/notes_backend/models/note.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const noteSchema = mongoose.Schema(
+    {
+        content: {
+            type: String,
+            minLength: 5,
+            required: true
+        },
+        important: Boolean
+    }
+)
+
+noteSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+    } }
+)
+
+module.exports = mongoose.model("Note", noteSchema)
